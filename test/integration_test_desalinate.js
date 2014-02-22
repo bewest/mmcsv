5d42dbfc73808cd346c658cf6b992035954f50b1
Ben West <bewest@gmail.com>
2014-02-21 16:37:32 -0800

    stub out initial desalinate method
    Simply pass through all parsed smbg, cbg, and carb records.
    Those match our standard data model.

---
+++ test/integration_test_desalinate.js
@@ -0,0 +1,29 @@
+var should = require('should');
+var fs = require('fs');
+var es = require('event-stream');
+var validators = require('tidepool-data-model');
+
+describe("desalinate", function() {
+
+  var desalinate = require('../lib/desalinate');
+	it('should exist and be callable', function( ) {
+    should.exist(desalinate);
+    should.exist(desalinate.call);
+  });
+    
+  describe("stream", function ( ) {
+    it('has results', function ( ) {
+      function proof (err, results) {
+          results.length.should.equal(21);
+          var report = validator(results);
+          report.errors.should.be.empty;
+      }
+
+      var input = fs.createReadStream('test/1days_smbg_basal_bolus_carbs.csv');
+      var validator = validators( );
+      var stream = es.pipeline(input, desalinate( ), es.writeArray(proof));
+    });
+
+  });
+
+});
