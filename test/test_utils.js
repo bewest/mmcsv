02134e90dd240b2e7195c4f3065a9843378f6bd8
Ben West <bewest@gmail.com>
2014-02-23 14:26:39 -0800

    Add helper utility for parsing the Raw-Value field

--- test/test_utils.js
+++ test/test_utils.js
@@ -14,4 +14,28 @@ describe("utils", function ( ) {
     }
     es.pipeline(input, utils.validate( ), es.writeArray(testEmpty));
   });
+
+  it("details should parse details", function ( ) {
+    var raw = '3214,2/9/14,20:45:16,2/9/14 20:45:16,,,,,,,,,,,,,,,5.1,125,106,13,45,64,137,0.2,4.9,0.0,,,,,,BolusWizardBolusEstimate,"BG_INPUT=137, BG_UNITS=mg dl, CARB_INPUT=64, CARB_UNITS=grams, CARB_RATIO=13, INSULIN_SENSITIVITY=45, BG_TARGET_LOW=106, BG_TARGET_HIGH=125, BOLUS_ESTIMATE=5.1, CORRECTION_ESTIMATE=0.2, FOOD_ESTIMATE=4.9, UNABSORBED_INSULIN_TOTAL=0, UNABSORBED_INSULIN_COUNT=0, ACTION_REQUESTOR=pump",12354037351,52963854,110,Paradigm 522';
+    var fields = utils.fields(raw);
+    fields.length.should.equal(39);
+    var values = utils.select(fields, 'Raw-Values');
+    values.should.equal('BG_INPUT=137, BG_UNITS=mg dl, CARB_INPUT=64, CARB_UNITS=grams, CARB_RATIO=13, INSULIN_SENSITIVITY=45, BG_TARGET_LOW=106, BG_TARGET_HIGH=125, BOLUS_ESTIMATE=5.1, CORRECTION_ESTIMATE=0.2, FOOD_ESTIMATE=4.9, UNABSORBED_INSULIN_TOTAL=0, UNABSORBED_INSULIN_COUNT=0, ACTION_REQUESTOR=pump');
+    var details = utils.details(values);
+    var correct = { BG_INPUT: '137',
+      BG_UNITS: 'mg dl',
+      CARB_INPUT: '64',
+      CARB_UNITS: 'grams',
+      CARB_RATIO: '13',
+      INSULIN_SENSITIVITY: '45',
+      BG_TARGET_LOW: '106',
+      BG_TARGET_HIGH: '125',
+      BOLUS_ESTIMATE: '5.1',
+      CORRECTION_ESTIMATE: '0.2',
+      FOOD_ESTIMATE: '4.9',
+      UNABSORBED_INSULIN_TOTAL: '0',
+      UNABSORBED_INSULIN_COUNT: '0',
+      ACTION_REQUESTOR: 'pump' };
+    details.should.eql(correct);
+  });
 });
